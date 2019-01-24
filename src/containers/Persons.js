465d888acc702e7a787f5d82983bed28c71394e4
Mihai Enuica <mihai.enuica@gmail.com>
2019-01-24 10:50:09 +0200

    S14 - initial commit - Assignment 2 add random age people, state in components

---
+++ src/containers/Persons.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+import Person from '../components/Person/Person';
+import AddPerson from '../components/AddPerson/AddPerson';
+
+class Persons extends Component {
+    state = {
+        persons: []
+    }
+
+    personAddedHandler = () => {
+        const newPerson = {
+            // Math.random() produce a random float number in range from 0 to 1 (exclusive of 1)
+            // Math.floor() function returns the largest integer less than or equal to a given number.
+            id: Math.random(), // not really unique but good enough here!
+            name: 'Mihai',
+            age: Math.floor( Math.random() * 40 )
+        }
+        this.setState( ( prevState ) => {
+            return { persons: prevState.persons.concat(newPerson)}
+        } );
+    }
+
+    personDeletedHandler = (personId) => {
+        this.setState( ( prevState ) => {
+            return { persons: prevState.persons.filter(person => person.id !== personId)}
+        } );
+    }
+
+    render () {
+        return (
+            <div>
+                <AddPerson personAdded={this.personAddedHandler} />
+                {this.state.persons.map(person => (
+                    <Person 
+                        key={person.id}
+                        name={person.name} 
+                        age={person.age} 
+                        clicked={() => this.personDeletedHandler(person.id)}/>
+                ))}
+            </div>
+        );
+    }
+}
+
+export default Persons;
